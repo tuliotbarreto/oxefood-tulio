4eba0ace79e6e0e367e66010dc59c999a291f898
aluno <aluno@ifpe>
2022-09-29 09:11:39 -0300

    enviando os arquivos para o git dos bglh que baiaxmos

---
+++ src/main/java/br/com/ifpe/oxefoodtulio/OxefoodTulioApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodtulio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodTulioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodTulioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodtulio/OxefoodTulioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodtulio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodTulioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
